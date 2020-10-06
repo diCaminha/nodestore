4e389ff57497a71797be2b866287c4087981e801
Denis <dineresc@gmail.com>
2020-10-06 19:16:29 -0300

    redirect form

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/product',(req, res, next) => {
-    res.send("<h1>Hello from /product </h1>")
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"> <input type="text" name="title"> <button type="submit" value="Submit"></button></form> ');
 });
 
-app.use('/',(req, res, next) => {
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     res.send("<h1>Hello from / </h1>")
 });
 
