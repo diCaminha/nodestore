7a5e115faf3134b2672c38c6264259302870d5be
Denis <dineresc@gmail.com>
2020-10-07 18:07:20 -0300

    add routers file

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"> <input type="text" name="title"> <button type="submit" value="Submit"></button></form> ');
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send("<h1>Hello from / </h1>")
-});
 
 app.listen(3000);
