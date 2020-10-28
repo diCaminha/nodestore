3d7832cbef5f077b12542c9bb3a5c6ce880aa662
Denis <dineresc@gmail.com>
2020-10-28 01:51:49 -0300

    updating the project

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,29 @@
 const path = require('path');
-const rootPath = require('../utils/paths');
+
 const express = require('express');
-const { RSA_NO_PADDING } = require('constants');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootPath,'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    products.push({'title': req.body.title});
-    res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-exports.router = router;
+exports.routes = router;
 exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,22 @@
 const path = require('path');
-const rootPath = require('../utils/paths');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-const adminData = require('../routes/admin');
-
 router.get('/', (req, res, next) => {
-    //logar dados de products
-    console.log(adminData.products);
-    res.sendFile(path.join(rootPath, 'views','shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
