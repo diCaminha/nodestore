0a3d0c3c8828cd515756e6986b469b56ffa9d29e
Denis <dineresc@gmail.com>
2020-10-28 18:22:14 -0300

    adding comments

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//configuring express instance
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//configuring routes and data
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+//refering routes
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
