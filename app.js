b63754949b08fd5dfe2a4b2ea5f1ff6afe891e8a
Denis <dineresc@gmail.com>
2020-10-29 18:38:40 -0300

    improving the project

--- app.js
+++ app.js
@@ -3,26 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const cartRoutes = require('./routes/cart');
 const errorController = require('./controllers/error');
 
 const app = express();
 
-
-//configuring express instance
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//refering routes
 app.use('/admin', adminRoutes);
-app.use('/cart', cartRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.errorPage);
+app.use(errorController.get404);
 
 app.listen(3000);
