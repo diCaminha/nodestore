43c7fe358ffe627ea80d4a0cb51fa0cd9258e81c
Denis <dineresc@gmail.com>
2020-10-30 13:22:44 -0300

    configure db

--- models/product.js
+++ models/product.js
@@ -1,24 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,15 +19,18 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
+  
+  }
+
+  static deleteById(id) {
+   
   }
 
   static fetchAll(cb) {
-    getProductsFromFile(cb);
+   
+  }
+
+  static findById(id, cb) {
+    
   }
 };
